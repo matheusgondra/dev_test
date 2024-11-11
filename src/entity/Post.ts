626fadab2b0c6490abf913d677792fc526c8ac46
matheusgondra <matheusgondra951@gmail.com>
2024-11-11 19:40:04 -0300

    feat: create Post entity

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,27 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
 
 //TODO Crie a entidade de Post
+@Entity()
+export class Post {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({ length: 100 })
+	title: string;
+
+	@Column({ length: 100 })
+	description: string;
+
+	@Column()
+	userId: number;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+
+	@ManyToOne(() => User, user => user.posts, { onDelete: "CASCADE" })
+	user: User;
+}
