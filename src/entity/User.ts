4b0289006d2234b1a98e8ddc068764eb0074d412
matheusgondra <matheusgondra951@gmail.com>
2024-11-11 19:39:42 -0300

    feat: create User entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,27 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany, JoinColumn } from "typeorm";
+import { Post } from "./Post";
 
 //TODO Crie a entidade de User
+@Entity()
+export class User {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({ length: 100 })
+	firstName: string;
+	
+	@Column({ length: 100 })
+	lastName: string;
+	
+	@Column({ length: 100, unique: true })
+	email: string;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+
+	@OneToMany(() => Post, post => post.user)
+	posts: Post[];
+}
